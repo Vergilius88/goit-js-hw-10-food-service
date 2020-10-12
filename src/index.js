f4a92e3237c8f4ae26f905ab98e4083d1895d748
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-10-12 18:19:45 +0300

    fixed ref

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import './styless/styles.scss';
+import menu from './menu.json';
+import menuTemplate from './template/menu-template.hbs';
+
+const bodyRef = document.querySelector('body');
+const switchInputRef = document.querySelector('#theme-switch-toggle');
+const menuRef = document.querySelector('.js-menu');
+const markup = menuTemplate(menu);
+
+menuRef.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const checkboxPosition = () => {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    switchInputRef.setAttribute("checked", true);
+  } 
+};
+const defaultTheme = () => {
+  if (localStorage.getItem('theme') === null) {
+    bodyRef.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  } else {
+    bodyRef.classList.add(localStorage.getItem('theme'));
+    checkboxPosition();
+  }
+};
+
+const changeThemeBySwitch = () => {
+  if (localStorage.getItem('theme') === Theme.LIGHT) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+};
+
+
+
+switchInputRef.addEventListener('change', changeThemeBySwitch);
+
+
+defaultTheme();
