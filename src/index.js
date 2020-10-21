5d0b1f6f90f8f72a0a135a3873a9a9a4672a3420
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-10-21 21:14:38 +0300

    fixed gaps

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './styless/styles.scss';
 import menu from './menu.json';
 import menuTemplate from './template/menu-template.hbs';
-
 const bodyRef = document.querySelector('body');
 const switchInputRef = document.querySelector('#theme-switch-toggle');
 const menuRef = document.querySelector('.js-menu');
 const markup = menuTemplate(menu);
-
 menuRef.insertAdjacentHTML('beforeend', markup);
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
@@ -27,7 +24,6 @@ const defaultTheme = () => {
     checkboxPosition();
   }
 };
-
 const changeThemeBySwitch = () => {
   if (localStorage.getItem('theme') === Theme.LIGHT) {
     bodyRef.classList.remove(Theme.LIGHT);
@@ -39,10 +35,5 @@ const changeThemeBySwitch = () => {
     localStorage.setItem('theme', Theme.LIGHT);
   }
 };
-
-
-
 switchInputRef.addEventListener('change', changeThemeBySwitch);
-
-
 defaultTheme();
